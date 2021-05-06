fe9ada5bf541cd5509e1583bc38cfdc2bfe7c029
elainicristina <elaini@tuta.io>
2021-05-06 16:54:28 -0300

    Atividade sobre funcoes

---
+++ semana1/funcoes/index.js
@@ -0,0 +1,108 @@
+// Exercícios de interpretação de código
+
+// ------> EXERCÍCIO 1 <------------
+
+/*
+a. O resultado da multiplicação entre 2*5 e 10*5.
+
+b. Resultado vai ser undefined pois mesmo com o return precisa
+de um valor para fazer a função rodar e também precisa invocar
+a função.
+
+*/
+
+// ------> EXERCÍCIO 2 <------------
+
+/*
+a. Ele pede para o usario inserir um texto. Faz uma função para
+procurar o nome "cenoura" e retornar true ou false, retornar
+as palavras em minuscula.
+
+b. Vai retornar true pois todas tem a palavra cenoura.
+
+*/
+
+// -------> Exercícios de escrita de código <------------
+
+// -------------> EXERCÍCIO 1 <------------
+
+
+// function informacoes () {
+//     console.log("Elaini, 20 anos, moro em teresina e sou estudante Labenu.")
+// }
+
+// informacoes();
+
+// const nome = prompt("Whats your name")
+// const years = prompt("How old are you?")
+// const city = prompt("What is tour city?")
+// const profession = prompt("What is your profession?")
+
+// function informocaes (nome, years, city, profession) {
+//     console.log(`Eu sou ${nome}, tenho ${years} anos, moro em ${city} e sou ${profession}. `)
+// }
+
+// informocaes(nome, years, city, profession);
+
+
+// -------------> EXERCÍCIO 2 <------------
+
+/*
+const numberOne = Number(prompt("Type a number."))
+const numberTwo = Number(prompt("Type a number."))
+
+function somaDosNumber(numberOne, numberTwo) {
+    const soma = numberOne + numberTwo
+    return soma
+}
+
+console.log(somaDosNumber(2, 8));
+
+
+const maior = function(numberOne, numberTwo) {
+    return numberOne >= numberTwo
+}
+
+console.log(maior(numberOne, numberTwo))
+
+
+function numberPar(numero) {
+    const restoDeDivisao = numero % 2
+    let resultado = restoDeDivisao === 0
+    return resultado
+}
+
+
+*/
+// let frase = "Eu amo a Labenu_"
+
+// const tamanhoFrase = function (frasee) {
+//     return frase.length
+// }
+// const resposta = tamanhoFrase(frase)
+// console.log(resposta)
+// console.log(frase.toUpperCase())
+
+
+// -------------> EXERCÍCIO 3 <------------
+
+function soma() {
+   return 30 + 3
+}
+// console.log(soma(30, 3))
+
+function subtracao() {
+    return 30 - 3
+}
+// console.log(subtracao(30, 3))
+
+function multiplicação() {
+    return 30 * 3
+}
+// console.log(multiplicação(30, 3))
+
+function divisao() {
+    return 30 / 3
+}
+// console.log(divisao(30, 3))
+
