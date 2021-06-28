5f0b2576a1b4a5f9c0c678db67f51ad38967d6cc
Elaini Cristina <elaini@tuta.io>
2021-06-28 17:06:04 -0300

    Atividade

---
+++ semana10/semana10-aulaHook/insta-lab-hooks/src/components/Post/Post.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { PostContainer, PostHeader, UserPhoto, PostPhoto, PostFooter, CommentContainer } from './styles'
+
+import IconeComContador from '../IconeComContador/IconeComContador'
+import SecaoComentario from '../SecaoComentario/SecaoComentario'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+
+const Post = (props) => {
+  const [curtido, setCurtido] = useState(false)
+  const [numeroDeCurtidas, setNumerosCurtidas] = useState(0)
+  const [comentando, setComentando] = useState(false)
+  const [numerosComentarios, setNumerosComentarios] = useState(0)
+  const [comentarios, setComentarios] = useState([])
+
+  const onClickCurtida = () => {
+    if({curtido}) {
+        setCurtido(!curtido)
+        setNumerosCurtidas(numeroDeCurtidas -1)
+    }
+    else {
+      setCurtido(!curtido)
+      setNumerosCurtidas(numeroDeCurtidas +1)
+    }
+  };
+
+  const onClickComentario = () => {
+    setComentando(!comentando)
+  };
+
+  const enviarComentario = (comentario) => {
+    const listaDeComentarios = [...comentarios, comentario]
+
+   
+      setComentarios(listaDeComentarios)
+      setComentando(false)
+      setNumerosComentarios(numerosComentarios + 1)
+ 
+  }
+
+  return (
+    <PostContainer>
+      <PostHeader>
+        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          onClickIcone={onClickCurtida}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+        />
+      </PostFooter>
+
+    </PostContainer>
+  )
+}
+
+export default Post
