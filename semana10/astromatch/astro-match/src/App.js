caa87dd5c836118ea59e6d52ed27c7dceb9a0d02
Elaini Cristina <elaini@tuta.io>
2021-06-30 19:24:13 -0300

    adicionando background color e spinner de loading

--- semana10/astromatch/astro-match/src/App.js
+++ semana10/astromatch/astro-match/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import Pessoas from './Inicial/AdcPessoa'
+import Pessoas from './Inicial/AdcPessoa';
+import styled from "styled-components";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+
+const RootDiv = styled.div `
+  min-height: 100vh;
+`
+
 
 const App = () => {
   return (
-    <div>
+    <RootDiv className='pt-5 pe-3 pb-5 ps-3 bg-dark'>
         <Pessoas/>
-    </div>
+    </RootDiv>
   );
 }
 
--- semana10/astromatch/astro-match/src/Inicial/AdcPessoa.js
+++ semana10/astromatch/astro-match/src/Inicial/AdcPessoa.js
@@ -3,67 +3,73 @@ import axios from 'axios'
 import styled from 'styled-components'
 
 const ImagemCard = styled.img `
-    width: 300px;
-    heigth: 100vh;
-    margin: auto;
+    height: 300px;
+    object-fit: cover;
+    object-position: top;
 `
 const Conteiner = styled.div `
-    width: 450px;
+    max-width: 450px;
     margin: auto;
-    margin-top: 70px;
 
+`
+const Spinner = styled.div `
+    position: absolute;
+    top: 50%;
+    left: 50%;
 `
 
 const TextoBody = styled.div `
     text-align: center;
-    color: gray;
 `
 
    function Pessoas() {
-    const [infor, setInformações] = useState({})
-    const [error, setError] = useState("")
-    const [recendo, setRecebendo] = useState(true)
+        const [infor, setInformações] = useState({})
+        const [error, setError] = useState("")
+        const [recendo, setRecebendo] = useState(true)
       
     
-    useEffect(() => {
-        axios
-        .get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/elaini-coelho-molina/person")
-        
-        .then((res) => {
-        console.log(res.data.profile)
-        setInformações(res.data.profile)
-        setRecebendo(false)
+        useEffect(() => {
+            axios
+            .get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/elaini-coelho-molina/person")
+            
+            .then((res) => {
+            console.log(res.data.profile)
+            setInformações(res.data.profile)
+            setRecebendo(false)
 
-    }).catch((error) => {
-        console.log(error.data)
-        setError(error.message)
-        setRecebendo(false)
-    })
-    }, [])
+        }).catch((error) => {
+            console.log(error.data)
+            setError(error.message)
+            setRecebendo(false)
+        })
+        }, [])
 
-    if(recendo) {
+        if(recendo) {
+            return(
+                <Spinner className="spinner-border text-light" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            )
+        }
+
+        if(error){
+            return <h1>Algo deu errado: {error}</h1>
+        }
+
         return(
-            <h1>Buscando parceiros ...</h1>
+            <Conteiner> 
+
+                <div className="card mb-3 shadow-lg border border-dark rounded-lg">
+                    <ImagemCard src={infor.photo} className="card-img-top" alt={infor.name}/>
+                    <TextoBody className="card-body">
+                        <h5 className="card-title">{infor.name}</h5>
+                        <p className="card-text">{infor.age }</p>
+                        <p className="card-text"><small className="text-muted">{infor.bio }</small></p>
+                    </TextoBody>
+                </div>
+            </Conteiner>
+
         )
-    }
-
-    if(error){
-        return <h1>Algo deu errado: {error}</h1>
-    }
-    return(
-        <Conteiner> 
-            <div className="card mb-3">
-                <ImagemCard src={infor.photo} className="card-img-top" alt={infor.name}/>
-
-                <TextoBody className="card-body">
-                    <h2 className="card-title">{infor.name}</h2>
-                    <h4 className="card-text">{infor.age }</h4>
-                    <h6 className="card-text">{infor.bio }</h6>
-                </TextoBody>
-
-            </div>
-        </Conteiner>
-    )
   
 }
 
