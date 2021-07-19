a0a1a229c26ca60b72af64739ba4b9498ffa8401
Elaini Cristina <elaini@tuta.io>
2021-07-19 15:56:11 -0300

    Page

---
+++ REVM3/receitas-app/src/pages/RecipeDetalisPage/RecipeDetals.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecipesDetalsPage = () => {
+    return(
+        <div>
+            RecipesDetalsPage
+        </div>
+    )
+}
+
+export default RecipesDetalsPage;
